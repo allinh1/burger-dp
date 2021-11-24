06dbc64153f77637fba9911a298a356ee72a1b03
allinh1 <dl.nguyen1322@gmail.com>
2021-11-24 15:06:04 +1100

    added builder constructor

--- src/BurgerTruck/Builder/BurgerBuilder.java
+++ src/BurgerTruck/Builder/BurgerBuilder.java
@@ -1,4 +1,22 @@
 package BurgerTruck.Builder;
 
-public class BurgerBuilder {
+public abstract class BurgerBuilder {
+
+    protected Burger burger;
+
+    public Burger getBurger() {
+        return burger;
+    }
+
+    public void createNewBurger() {
+        burger = new Burger();
+    }
+
+    public abstract void buildBun();
+
+    public abstract void buildSauce();
+
+    public abstract void buildPatty();
+
+    public abstract void buildToppings();
 }
--- src/BurgerTruck/Builder/Server.java
+++ src/BurgerTruck/Builder/Server.java
@@ -1,4 +1,25 @@
 package BurgerTruck.Builder;
 
 public class Server {
+//    private PizzaBuilder pizzaBuilder;
+//
+//    public void setPizzaBuilder(PizzaBuilder pizzaBuilder) {
+//        this.pizzaBuilder = pizzaBuilder;
+//    }
+//
+//    public PizzaBuilder getPizzaBuilder() {
+//        return pizzaBuilder;
+//    }
+//
+//    public Pizza getPizza() {
+//        return pizzaBuilder.getPizza();
+//    }
+//
+//    public void constructPizza() {
+//        pizzaBuilder.createNewPizza();
+//        pizzaBuilder.buildCrust();
+//        pizzaBuilder.buildSauce();
+//        pizzaBuilder.buildToppings();
+//    }
+
 }
